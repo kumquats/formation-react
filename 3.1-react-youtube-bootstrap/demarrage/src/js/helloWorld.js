fb8c92c0cf560da0c9cecdbee9d9946cd2b37689
thomas <tf@kumquats.fr>
2017-11-26 20:24:28 +0100

    nouvelle structure tp src+site

---
+++ 3.1-react-youtube-bootstrap/demarrage/src/js/helloWorld.js
@@ -0,0 +1,4 @@
+const helloWorld = () => {
+	console.log( 'Hello world !');
+}
+export default helloWorld;
---
+++ 3.1-react-youtube-bootstrap/demarrage/src/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+	// Fichier d'entrée
+	entry: './js/app.js',
+	// Fichier de sortie
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/, // tous les fichiers .js
+				exclude: /node_modules/, // sauf le dossier node_modules
+				use: { // seront transpilés par babel
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true // accélère la génération des JS en cachant les transpilations
+					}
+				}
+			}
+		]
+	},
+	devtool: 'source-map',
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			},
+		}),
+	]
+}
