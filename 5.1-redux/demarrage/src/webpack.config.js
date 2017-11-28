45d7f4a2b71aa827a5ef3d2968e98704daab8785
thomas <tf@kumquats.fr>
2017-11-28 15:06:48 +0100

    5.1 : maj demarrage

---
+++ 5.1-redux/demarrage/src/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+module.exports = {
+	// Fichier d'entrée
+	entry: './js/app.js',
+	// Fichier de sortie
+	output: {
+		path: path.resolve(__dirname, '../site/web/js'),
+		filename: 'app.bundle.js'
+	},
+	externals: {
+        'config': 'config',
+    },
+	module: {
+		rules: [
+			{
+				test: /\.js$/, // tous les fichiers .js
+				exclude: /node_modules/, // sauf le dossier node_modules
+				use: { // seront transpilés par babel
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true // accélère la génération des JS en cachant les transpilations
+					}
+				}
+			}
+		]
+	},
+	devtool: 'source-map',
+	// plugins: [
+	// 	new webpack.optimize.UglifyJsPlugin({
+	// 		compress: {
+	// 			warnings: false,
+	// 		},
+	// 		output: {
+	// 			comments: false,
+	// 		},
+	// 	}),
+	// ]
+}
